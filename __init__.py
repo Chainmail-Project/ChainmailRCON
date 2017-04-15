6ec3fc1c7d9d3df4ba8a529f56b89677b2bd59df
Riley Flynn <riley@rileyflynn.me>
2017-04-15 13:15:33 -0230

    Added command registration and made the plugin instance available globally

--- __init__.py
+++ __init__.py
@@ -1,6 +1,8 @@
+import builtins
 import json
 import os
 import random
+import re
 import string
 
 from Chainmail import Wrapper
@@ -8,8 +10,11 @@ from Chainmail.Plugin import ChainmailPlugin
 
 
 class ChainmailRCON(ChainmailPlugin):
+
     def __init__(self, manifest: dict, wrapper: "Wrapper.Wrapper") -> None:
         super().__init__(manifest, wrapper)
+
+        self.commands = []
 
         if not os.path.isfile(os.path.join(manifest["path"], "config.json")):
             with open(os.path.join(manifest["path"], "config.json"), "w") as f:
@@ -23,3 +28,22 @@ class ChainmailRCON(ChainmailPlugin):
         else:
             with open(os.path.join(manifest["path"], "config.json")) as f:
                 self.config = json.load(f)
+
+        builtins.RCON = self
+
+    def register_command(self, name: str, regex: str, description: str, handler: classmethod, requires_auth: bool = False) -> None:
+        """
+        Registers a new RCON command
+        :param name: The name of the RCON command. Typically used for command lists
+        :param regex: The regex used to process the command
+        :param description: The description of the command
+        :param handler: The handler for the command
+        :param requires_auth: Whether the command requires the client to be authenticated
+        """
+        self.commands.append({
+            "name": name,
+            "regex": re.compile(regex),
+            "description": description,
+            "handler": handler,
+            "requires_auth": requires_auth
+        })
