191e6106cfcf44142b189288ca5dba01702a5ceb
Riley Flynn <riley@rileyflynn.me>
2017-04-15 13:04:35 -0230

    Added config generation

--- __init__.py
+++ __init__.py
@@ -1,5 +1,25 @@
+import json
+import os
+import random
+import string
+
+from Chainmail import Wrapper
 from Chainmail.Plugin import ChainmailPlugin
 
 
 class ChainmailRCON(ChainmailPlugin):
-    pass
+    def __init__(self, manifest: dict, wrapper: "Wrapper.Wrapper") -> None:
+        super().__init__(manifest, wrapper)
+
+        if not os.path.isfile(os.path.join(manifest["path"], "config.json")):
+            with open(os.path.join(manifest["path"], "config.json"), "w") as f:
+                self.config = {
+                    "password": "".join(random.choice(string.ascii_letters + string.digits) for i in range(16)),
+                    "use_whitelist": False,
+                    "whitelisted_ips": []
+                }
+                json.dump(self.config, f, sort_keys=True, indent=4)
+                self.logger.info(f"Generated new config. Use the password {self.config['password']} to authenticate.")
+        else:
+            with open(os.path.join(manifest["path"], "config.json")) as f:
+                self.config = json.load(f)
